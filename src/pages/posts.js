e1ca1a92282083ff605ceb6f456b1789b060a151
Paul Frazee <pfrazee@gmail.com>
2015-02-11 22:03:59 -0600

    added infinite scroll to message feed

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -15,19 +15,21 @@ module.exports = function (app) {
 
     // markup
    
+    var feedTBody = h('tbody',
+      msgs.map(function (msg) { 
+        if (msg.value) return com.messageSummary(app, msg, mustRenderOpts)
+      }))
+    var feedContainer = h('.message-feed-container', { onscroll: onscroll },
+      h('table.message-feed',
+        h('thead',
+          h('tr',
+            h('td.text-right', 'author'), h('td', 'type'), h('td', 'content'), h('td', com.icon('comment')), h('td', com.icon('paperclip')), h('td', 'age'))),
+        feedTBody))
     app.setPage('posts', h('.row',
       h('.col-xs-2.col-md-1', com.sidenav(app)),
       h('.col-xs-10.col-md-9', 
         h('p#get-latest.hidden', h('button.btn.btn-primary.btn-block', { onclick: app.refreshPage }, 'Get Latest')),
-        h('.message-feed-container',
-          h('table.message-feed',
-            h('thead',
-              h('tr',
-                h('td.text-right', 'author'), h('td', 'type'), h('td', 'content'), h('td', com.icon('comment')), h('td', com.icon('paperclip')), h('td', 'age'))),
-            h('tbody',
-              msgs.map(function (msg) { 
-                if (msg.value) return com.messageSummary(app, msg, mustRenderOpts)
-              }))))
+        feedContainer
         //com.introhelp(app)
       ),
       h('.hidden-xs.hidden-sm.col-md-2',
@@ -36,6 +38,26 @@ module.exports = function (app) {
         com.sidehelp(app)
       )
     ))
+
+    // handlers
+
+    var fetching = false
+    function onscroll (e) {
+      if (fetching)
+        return
+      if (feedContainer.offsetHeight + feedContainer.scrollTop >= feedContainer.scrollHeight) {
+        fetching = true
+        app.ssb.phoenix.getFeed({ lt: msgs[msgs.length - 1], reverse: true }, function (err, _msgs) {
+          fetching = false
+          if (_msgs && _msgs.length) {
+            msgs = _msgs
+            msgs.forEach(function (msg) { 
+              if (msg.value) feedTBody.appendChild(com.messageSummary(app, msg, mustRenderOpts))
+            })
+          }
+        })
+      }
+    }
       
   })
 }
