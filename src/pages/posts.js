d1929594e1564865e2a9752be5d0b13fa069c567
Paul Frazee <pfrazee@gmail.com>
2015-02-11 22:13:46 -0600

    removed right column from feed

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -27,16 +27,16 @@ module.exports = function (app) {
         feedTBody))
     app.setPage('posts', h('.row',
       h('.col-xs-2.col-md-1', com.sidenav(app)),
-      h('.col-xs-10.col-md-9', 
+      h('.col-xs-10.col-md-11', 
         h('p#get-latest.hidden', h('button.btn.btn-primary.btn-block', { onclick: app.refreshPage }, 'Get Latest')),
         feedContainer
         //com.introhelp(app)
-      ),
-      h('.hidden-xs.hidden-sm.col-md-2',
+      )
+      /*h('.hidden-xs.hidden-sm.col-md-2',
         com.adverts(app),
         h('hr'),
         com.sidehelp(app)
-      )
+      )*/
     ))
 
     // handlers
