b52b6e20cdb61d697ace84e0cd9fa8eb8b195b31
Paul Frazee <pfrazee@gmail.com>
2015-02-24 13:01:03 -0600

    move reply btn right on message thread

--- src/com/message-thread.js
+++ src/com/message-thread.js
@@ -57,10 +57,10 @@ module.exports = function (app, thread, opts) {
   var threadInner = h(viz.cls,
     h('ul.threadmeta.list-inline',
       h('li.type', com.icon(viz.icon)),
-      h('li.button', h('a', { href: '#', onclick: onreply }, 'Reply')),
       h('li', com.userlink(thread.value.author, app.names[thread.value.author]), com.nameConfidence(thread.value.author, app)),
       h('li', com.a('#/', u.prettydate(new Date(thread.value.timestamp), true), { title: 'View message thread' })),
       h('li.in-response-to'), // may be populated by the message page
+      h('li.button.strong.pull-right', h('a', { href: '#', onclick: onreply }, 'Reply')),
       h('li.button.pull-right', h('a', { href: '/msg/'+thread.key, target: '_blank' }, 'as JSON'))),
     h('.message.top', content),
     h('.attachments', attachments),
