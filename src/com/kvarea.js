a9fe4b5a553afd885416db0c598a6301e2baedfd
Paul Frazee <pfrazee@gmail.com>
2015-02-28 13:52:03 -0600

    adding text to the last row now causes a new row to be formed

--- src/com/kvarea.js
+++ src/com/kvarea.js
@@ -1,21 +1,40 @@
 var h = require('hyperscript')
 
 module.exports = function (app, value) {
-  var kvarea = h('table.kvarea')
+
+  // markup
+  
+  var kvarea = h('table.kvarea', { onkeyup: onkeyup })
   setValue(app, kvarea, value)
+
+  // handlers
+
+  function onkeyup (e) {
+    var tbody = kvarea.firstChild
+    var tr = e.target.parentNode
+    if (tr == tbody.lastChild) {
+      // does the last row have text in it?
+      if (tr.innerText.trim().length > 0) {
+        // add a row
+        tbody.appendChild(row('', ''))
+      }
+    }
+  }
+
   return kvarea
 }
 
 var setValue =
 module.exports.setValue = function (app, table, value) {
   table.innerHTML = ''
-  table.appendChild(h('tbody', rows(value)))
+  table.appendChild(h('tbody', rows(value), row('', '')))
+}
+
+function row (k, v) {
+  return h('tr', h('td', {contentEditable: true}, k), h('td', {contentEditable: true}, v))
 }
 
 function rows (obj, path) {
-  function row (k, v) {
-    return h('tr', h('td', {contentEditable: true}, path+k), h('td', {contentEditable: true}, v))
-  }
 
   var els = []
   path = (path) ? path + '.' : ''
@@ -23,7 +42,7 @@ function rows (obj, path) {
     if (obj[k] && typeof obj[k] == 'object')
       els = els.concat(rows(obj[k], path+k))
     else
-      els.push(row(k, ''+obj[k]))
+      els.push(row(path+k, ''+obj[k]))
 
   }
 
