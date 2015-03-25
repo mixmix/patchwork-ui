2071313edded0e67637e36cdbfd1b7f864c0329e
Paul Frazee <pfrazee@gmail.com>
2015-03-25 14:24:55 -0500

    tweaks to contact-summary coloring

--- src/com/contact-summary.js
+++ src/com/contact-summary.js
@@ -63,11 +63,21 @@ module.exports = function (app, profile, follows) {
   tmpImg.onload = function () {
     var rgb = u.getAverageRGB(tmpImg)
     if (rgb) {
+      // color-correct to try to go within 96-128 of average
       var avg = (rgb.r + rgb.g + rgb.b) / 3
       if (avg > 128) {
         rgb.r = (rgb.r/2)|0
         rgb.g = (rgb.g/2)|0
         rgb.b = (rgb.b/2)|0
+        avg = (rgb.r + rgb.g + rgb.b) / 3
+      }
+      while (avg < 96) {
+        var ratio = (96 - avg)/96 + 1
+        if (ratio < 1.2)
+          ratio = 1.2
+        rgb.r = (rgb.r*ratio)|0
+        rgb.g = (rgb.g*ratio)|0
+        rgb.b = (rgb.b*ratio)|0
         avg = (rgb.r + rgb.g + rgb.b) / 3
       }
       var rgb2 = { r: ((rgb.r/2)|0), g: ((rgb.g/2)|0), b: ((rgb.b/2)|0) }
@@ -76,11 +86,11 @@ module.exports = function (app, profile, follows) {
       try { title.querySelector('h3').style.color = 'rgba('+rgb2.r+','+rgb2.g+','+rgb2.b+', 0.75)' } catch (e) {}
       try { title.querySelector('p').style.color  = 'rgba('+rgb2.r+','+rgb2.g+','+rgb2.b+', 0.75)' } catch (e) {}
       function setColors (el) {
-        if (el.classList.contains('selected')) {
-          el.style.color = 'rgb('+rgb.r+','+rgb.g+','+rgb.b+')'
-          el.style.background = 'rgba('+rgb.r+','+rgb.g+','+rgb.b+',0.5)'
+        if (!el.classList.contains('selected')) {
+          el.style.color = 'rgba(255,255,255,0.35)'//'rgb('+rgb.r+','+rgb.g+','+rgb.b+')'
+          el.style.background = 'rgb('+rgb2.r+','+rgb2.g+','+rgb2.b+')'
         } else {
-          el.style.color = 'rgba(255,255,255,0.5)'//(avg < 128) ? 'rgba(255,255,255,0.5)' : 'rgba(0,0,0,0.5)'
+          el.style.color = 'rgba(255,255,255,0.5)'
           el.style.background = 'rgb('+rgb.r+','+rgb.g+','+rgb.b+')'
         }
       }
