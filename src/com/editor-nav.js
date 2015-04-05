3d1b5573c77d48c9e90e43e418d814baf386d280
Paul Frazee <pfrazee@gmail.com>
2015-04-04 19:50:54 -0500

    add save/load to program editor

--- src/com/editor-nav.js
+++ src/com/editor-nav.js
@@ -1,4 +1,5 @@
 var h = require('hyperscript')
+var buffers = require('ls-buffers')
 var com = require('./index')
 
 module.exports = function (app, opts) {
@@ -6,17 +7,17 @@ module.exports = function (app, opts) {
 
   // markup
 
-  return h('.editor-nav',
+  var nameInput = h('input.form-control', { placeholder: 'Name' })
+  var nav = h('.editor-nav',
     com.nav({
       current: 'file',
       items: [
         ['file',   ontab, 'File'],
-        ['files',  ontab, 'Files'],
         ['help',   ontab, 'Help']
       ]
     }),
     h('.editor-nav-body',
-      h('p', h('input.form-control', { placeholder: 'Name' }))))
+      h('p', nameInput)))
       // h('p', 
         // h('a.btn.btn-primary', com.icon('play')),
         // h('a.btn.btn-warning', com.icon('stop'))),
@@ -26,4 +27,15 @@ module.exports = function (app, opts) {
   // handlers
 
   var ontab = '' // :TODO:
+
+  // apis
+
+  nav.getName = function () {
+    return nameInput.value
+  }
+  nav.setName = function (v) {
+    nameInput.value = v
+  }
+
+  return nav
 }
--- src/pages/program-editor.js
+++ src/pages/program-editor.js
@@ -1,39 +1,54 @@
 'use strict'
 var h = require('hyperscript')
+var buffers = require('ls-buffers')
 var CodeMirror = require('codemirror')
 require('codemirror/mode/javascript/javascript')
 require('codemirror/keymap/sublime')
 var com = require('../com')
 var u = require('../lib/util')
 
+/**
+ * TODO
+ * - domstorage-blobspace
+ * - save ui
+ * - list/load ui
+ * - evaler
+ * - env globals
+ * - com registry
+ * - active com ui
+ */
+
 module.exports = function (app) {
 
   var hasChanges = false
+  var buff = buffers.load(app.page.param) || {id: null, meta:{}, text:''}
 
   // markup
 
-  var saveBtn = h('a', { href: '#/program-editor', 'data-overlay': 'Save' }, com.icon('floppy-disk'))
+  var saveBtn = h('a', { href: '#', onclick: onsave, 'data-overlay': 'Save' }, com.icon('floppy-disk'))
   var edContainer = h('.editor-container',
     h('.editor-ctrls',
       saveBtn,
       h('a', { href: '#/program-editor', 'data-overlay': 'Eval' }, com.icon('play'))))
-
+  var editorNav = com.editorNav(app, {  })
   app.setPage('new-program', h('.row',
     h('.col-xs-1', com.sidenav(app)),
     h('.col-xs-8', edContainer),
     h('.col-xs-3.right-column.full-height',
       h('.right-column-inner', com.notifications(app)),
-      com.editorNav(app, {  }),
+      editorNav,
       com.sidehelp(app))
   ))
 
   // post-render decoration
 
+  editorNav.setName(buff.meta.name || '')
   var editor = CodeMirror(function (el) {
     el.classList.add('full-height')
     edContainer.appendChild(el)
     el.style.height = (window.innerHeight - el.offsetTop) + 'px'
   }, {
+    value: buff.text,
     mode: 'javascript',
     theme: 'monokai',
     keyMap: 'sublime',
@@ -63,4 +78,19 @@ module.exports = function (app) {
 
   // handlers
 
+  function onsave (e) {
+    e.preventDefault()
+
+    console.log(editorNav.getName())
+    buff.meta.name = editorNav.getName()
+    buff.text = editor.getValue()
+    buffers.save(buff)
+
+    if (app.page.param != buff.id)
+      window.location = '#/program-editor/'+buff.id
+
+    editorNav.setName(buff.meta.name)
+    hasChanges = false
+    saveBtn.classList.remove('highlighted')
+  }
 }
