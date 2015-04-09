d912ff47b97828d1728074866b09d3f9eea9148a
Paul Frazee <pfrazee@gmail.com>
2015-04-09 11:01:16 -0500

    remove app.js, move into home.js

--- src/app-ui/index.js
+++ src/app-ui/index.js
@@ -1,91 +1,92 @@
 var schemas = require('ssb-msg-schemas')
 
-exports.showUserId = function () { 
-  swal('Here is your contact id', this.user.id)
-}
+module.exports = function (phoenix) {
 
-exports.setStatus = function (type, message) {
-  var status = document.getElementById('app-status')
-  status.innerHTML = ''
-  if (type)
-    status.appendChild(h('.alert.alert-'+type, message))
-}
-
-exports.followPrompt = function (e) {
-  var app = this
-  e.preventDefault()
-
-  var id = prompt('Enter the contact id or invite code')
-  if (!id)
-    return
-
-  // surrounded by quotes?
-  // the scuttlebot cli ouputs invite codes with quotes, so this could happen
-  if (id.charAt(0) == '"' && id.charAt(id.length - 1) == '"')
-    id = id.slice(1, -1) // strip em
-
-  var parts = id.split(',')
-  var isInvite = (parts.length === 3)
-  if (isInvite) {
-    app.ui.setStatus('info', 'Contacting server with invite code, this may take a few moments...')
-    app.ssb.invite.addMe(id, next)
-  }
-  else
-    schemas.addContact(app.ssb, id, { following: true }, next)
-    
-  function next (err) {
-    app.ui.setStatus(false)
-    if (err) {
-      console.error(err)
-      if (isInvite)
-        swal('Invite Code Failed', userFriendlyInviteError(err.stack || err.message), 'error')
-      else
-        swal('Error While Publishing', err.message, 'error')
-    }
-    else {
-      if (isInvite)
-        swal('Invite Code Accepted', 'You are now hosted by '+parts[0], 'success')
-      else
-        swal('Contact Added', 'You will now follow the messages published by your new contact.', 'success')
-      app.refreshPage()
-    }
+  phoenix.ui.showUserId = function () { 
+    swal('Here is your contact id', this.user.id)
   }
 
-  function userFriendlyInviteError(msg) {
-    if (~msg.indexOf('incorrect or expired') || ~msg.indexOf('has expired'))
-      return 'Invite code is incorrect or expired. Make sure you copy/pasted it correctly. If you did, ask the pub-server owner for a new code and try again.'
-    if (~msg.indexOf('invalid') || ~msg.indexOf('feed to follow is missing') || ~msg.indexOf('may not be used to follow another key'))
-      return 'Invite code is malformed. Make sure you copy/pasted it correctly. If you did, ask the pub-server owner for a new code and try again.'
-    if (~msg.indexOf('pub server did not have correct public key'))
-      return 'The pub server did not identify itself correctly for the invite code. Ask the pub-server owner for a new code and try again.'
-    if (~msg.indexOf('unexpected end of parent stream'))
-      return 'Failed to connect to the pub server. Check your connection, make sure the pub server is online, and try again.'
-    if (~msg.indexOf('already following'))
-      return 'You are already followed by this pub server.'
-    return 'Sorry, an unexpected error occurred. Please try again.'
+  phoenix.ui.setStatus = function (type, message) {
+    var status = document.getElementById('app-status')
+    status.innerHTML = ''
+    if (type)
+      status.appendChild(h('.alert.alert-'+type, message))
   }
-}
 
-exports.setNamePrompt = function (userId) {
-  var app = this
-  app.ssb.whoami(function (err, user) {
-    userId = userId || user.id
-    var isSelf = user.id === userId
-    
-    var name = (isSelf) ?
-      prompt('What would you like your nickname to be?') :
-      prompt('What would you like their nickname to be?')
-    if (!name)
+  phoenix.ui.followPrompt = function (e) {
+    e.preventDefault()
+
+    var id = prompt('Enter the contact id or invite code')
+    if (!id)
       return
 
-    if (!confirm('Set nickname to '+name+'?'))
-      return
+    // surrounded by quotes?
+    // the scuttlebot cli ouputs invite codes with quotes, so this could happen
+    if (id.charAt(0) == '"' && id.charAt(id.length - 1) == '"')
+      id = id.slice(1, -1) // strip em
 
-    schemas.addContact(app.ssb, userId, { name: name }, done)
-
-    function done(err) {
-      if (err) swal('Error While Publishing', err.message, 'error')
-      else app.refreshPage()
+    var parts = id.split(',')
+    var isInvite = (parts.length === 3)
+    if (isInvite) {
+      phoenix.ui.setStatus('info', 'Contacting server with invite code, this may take a few moments...')
+      phoenix.ssb.invite.addMe(id, next)
     }
-  })
+    else
+      schemas.addContact(phoenix.ssb, id, { following: true }, next)
+      
+    function next (err) {
+      phoenix.ui.setStatus(false)
+      if (err) {
+        console.error(err)
+        if (isInvite)
+          swal('Invite Code Failed', userFriendlyInviteError(err.stack || err.message), 'error')
+        else
+          swal('Error While Publishing', err.message, 'error')
+      }
+      else {
+        if (isInvite)
+          swal('Invite Code Accepted', 'You are now hosted by '+parts[0], 'success')
+        else
+          swal('Contact Added', 'You will now follow the messages published by your new contact.', 'success')
+        phoenix.refreshPage()
+      }
+    }
+
+    function userFriendlyInviteError(msg) {
+      if (~msg.indexOf('incorrect or expired') || ~msg.indexOf('has expired'))
+        return 'Invite code is incorrect or expired. Make sure you copy/pasted it correctly. If you did, ask the pub-server owner for a new code and try again.'
+      if (~msg.indexOf('invalid') || ~msg.indexOf('feed to follow is missing') || ~msg.indexOf('may not be used to follow another key'))
+        return 'Invite code is malformed. Make sure you copy/pasted it correctly. If you did, ask the pub-server owner for a new code and try again.'
+      if (~msg.indexOf('pub server did not have correct public key'))
+        return 'The pub server did not identify itself correctly for the invite code. Ask the pub-server owner for a new code and try again.'
+      if (~msg.indexOf('unexpected end of parent stream'))
+        return 'Failed to connect to the pub server. Check your connection, make sure the pub server is online, and try again.'
+      if (~msg.indexOf('already following'))
+        return 'You are already followed by this pub server.'
+      return 'Sorry, an unexpected error occurred. Please try again.'
+    }
+  }
+
+  phoenix.ui.setNamePrompt = function (userId) {
+    phoenix.ssb.whoami(function (err, user) {
+      userId = userId || user.id
+      var isSelf = user.id === userId
+      
+      var name = (isSelf) ?
+        prompt('What would you like your nickname to be?') :
+        prompt('What would you like their nickname to be?')
+      if (!name)
+        return
+
+      if (!confirm('Set nickname to '+name+'?'))
+        return
+
+      schemas.addContact(phoenix.ssb, userId, { name: name }, done)
+
+      function done(err) {
+        if (err) swal('Error While Publishing', err.message, 'error')
+        else phoenix.refreshPage()
+      }
+    })
+  }
 }
--- src/home.js
+++ src/home.js
@@ -1,39 +1,244 @@
 'use strict'
-var muxrpc       = require('muxrpc')
-var Serializer   = require('pull-serializer')
-var auth         = require('ssb-domain-auth')
+var muxrpc     = require('muxrpc')
+var Serializer = require('pull-serializer')
+var auth       = require('ssb-domain-auth')
+var h          = require('hyperscript')
+var multicb    = require('multicb')
+var router     = require('phoenix-router')
+var pull       = require('pull-stream')
+var emojis     = require('emoji-named-characters')
+var schemas    = require('ssb-msg-schemas')
+var channel    = require('ssb-channel')
+var com        = require('./com')
+var pages      = require('./pages')
+var u          = require('./lib/util')
+var addUI      = require('./app-ui')
 
-//the SSB_MANIFEST variable is created by /manifest.js
-//which is loaded before the javascript bundle.
+// program load
+setup()
+runPlugins()
 
-var ssb        = muxrpc(SSB_MANIFEST, false, function (stream) { return Serializer(stream, JSON, {split: '\n\n'}) })()
-var localhost  = require('ssb-channel').connect(ssb, 'localhost')
-var phoenix    = require('./app')(ssb)
+// create the application object and register handlers
+function setup() {
+  // the SSB_MANIFEST variable is created by /manifest.js, which is loaded before the javascript bundle.
+  var ssb = muxrpc(SSB_MANIFEST, false, function (stream) { return Serializer(stream, JSON, {split: '\n\n'}) })()
+  var localhost = channel.connect(ssb, 'localhost')
 
-localhost.on('connect', function() {
-  // authenticate the connection
-  auth.getToken(window.location.host, function(err, token) {
-    if (err) return localhost.close(), console.error('Token fetch failed', err)
-    ssb.auth(token, function(err) {
-      phoenix.ui.setStatus(false)
-      phoenix.setupRpcConnection()
-      phoenix.refreshPage()
+  // master state object
+  window.phoenix = {
+    ssb: ssb,
+    refreshPage: refreshPage,
+    setPage: setPage,
+
+    page: {
+      id: 'feed',
+      param: null,
+      qs: {}
+    },
+
+    ui: {
+      suggestOptions: { ':': [], '@': [] },
+      actionItems: null
+    },
+
+    user: {
+      id: null,
+      profile: null
+    },
+
+    users: {
+      names: null,
+      nameTrustRanks: null,
+      profiles: null
+    },
+
+    // for plugins
+    h: require('hyperscript'),
+    pull: require('pull-stream')
+  }
+  addUI(phoenix) // add ui methods
+
+  // events
+  window.addEventListener('hashchange', phoenix.refreshPage)
+  window.addEventListener('resize', resizeControls)
+  document.body.addEventListener('click', onClick)
+
+  // periodically poll and rerender the current connections
+  setInterval(pollPeers, 5000)
+
+  // options for the suggest box
+  for (var emoji in emojis) {
+    phoenix.ui.suggestOptions[':'].push({
+      image: '/img/emoji/' + emoji + '.png',
+      title: emoji,
+      subtitle: emoji,
+      value: emoji + ':'
+    })
+  }
+
+  // rpc connection
+  localhost.on('connect', function() {
+    // authenticate the connection
+    auth.getToken(window.location.host, function(err, token) {
+      if (err) return localhost.close(), console.error('Token fetch failed', err)
+      ssb.auth(token, function(err) {
+        phoenix.ui.setStatus(false)
+        setupRpcConnection()
+        phoenix.refreshPage()
+      })
     })
   })
-})
+  localhost.on('error', function(err) {
+    // inform user and attempt a reconnect
+    console.log('Connection Error', err)
+    phoenix.ui.setStatus('danger', 'Lost connection to the host program. Please restart the host program. Trying again in 10 seconds.')
+    localhost.reconnect()
+  })
+  localhost.on('reconnecting', function(err) {
+    console.log('Attempting Reconnect')
+    phoenix.ui.setStatus('danger', 'Lost connection to the host program. Reconnecting...')
+  })
+}
 
-localhost.on('error', function(err) {
-  // inform user and attempt a reconnect
-  console.log('Connection Error', err)
-  phoenix.ui.setStatus('danger', 'Lost connection to the host program. Please restart the host program. Trying again in 10 seconds.')
-  localhost.reconnect()
-})
+function runPlugins() {
+  u.getJson('/plugins.json', function (err, plugins) {
+    if (err) {
+      console.error('Failed to load plugins')
+      console.error(err)
+      if (plugins)
+        console.error(plugins)
+      return
+    }
+    for (var k in plugins.files) {
+      try {
+        console.log('Executing plugin', k)
+        eval('(function() {\n"use strict"\n\n'+plugins.files[k]+'\n\n})()')
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  })
+}
 
-localhost.on('reconnecting', function(err) {
-  console.log('Attempting Reconnect')
-  phoenix.ui.setStatus('danger', 'Lost connection to the host program. Reconnecting...')
-})
+function resizeControls() {
+  function rc (sel) {
+    var els = document.querySelectorAll(sel)
+    for (var i=0; i < els.length; i++)
+      els[i].style.height = (window.innerHeight - els[i].offsetTop) + 'px'
+  }
+  try { rc('.full-height') } catch (e) {}
+}
 
-phoenix.h      = require('hyperscript')
-phoenix.pull   = require('pull-stream')
-window.phoenix = phoenix
+function onClick (e) {
+  // look for link clicks which should trigger same-page refreshes
+  var el = e.target
+  while (el) {
+    if (el.tagName == 'A' && el.origin == window.location.origin && el.hash && el.hash == window.location.hash)
+      return e.preventDefault(), e.stopPropagation(), phoenix.refreshPage()
+    el = el.parentNode
+  }
+}
+
+// get peer status from sbot, update ui controls
+function pollPeers () {
+  var peersTables = Array.prototype.slice.call(document.querySelectorAll('table.peers tbody'))
+  if (!peersTables.length)
+    return // only update if peers are in the ui
+  phoenix.ssb.gossip.peers(function (err, peers) {
+    if (err)
+      return
+    peersTables.forEach(function (tb) {  
+      tb.innerHTML = ''
+      com.peers(phoenix, peers).forEach(function (row) {
+        tb.appendChild(row)
+      })
+    })
+  })
+}
+
+// should be called each time the rpc connection is (re)established
+function setupRpcConnection () {
+  pull(phoenix.ssb.phoenix.createEventStream(), pull.drain(function (event) {
+    if (event.type == 'message')
+      setNewMessageCount(getNewMessageCount() + 1)
+  }))
+}
+
+// re-renders the page
+function refreshPage (e) {
+  e && e.preventDefault()
+
+  // clear pending messages
+  setNewMessageCount(0)
+
+  // run the router
+  var route = router('#'+(location.href.split('#')[1]||''), 'feed')
+  phoenix.page.id    = route[0]
+  phoenix.page.param = route[1]
+  phoenix.page.qs    = route[2] || {}
+
+  // collect common data
+  var done = multicb({ pluck: 1 })
+  phoenix.ssb.whoami(done())
+  phoenix.ssb.phoenix.getNamesById(done())
+  phoenix.ssb.phoenix.getNameTrustRanks(done())
+  phoenix.ssb.phoenix.getAllProfiles(done())
+  phoenix.ssb.phoenix.getActionItems(done())
+  done(function (err, data) {
+    if (err) throw err.message
+    phoenix.user.id = data[0].id
+    phoenix.users.names = data[1]
+    phoenix.users.nameTrustRanks = data[2]
+    phoenix.users.profiles = data[3]
+    phoenix.ui.actionItems = data[4]
+    phoenix.user.profile = phoenix.users.profiles[phoenix.user.id]
+
+    // refresh suggest options for usernames
+    phoenix.ui.suggestOptions['@'] = []
+    for (var k in phoenix.users.profiles) {
+      var name = phoenix.users.names[k] || k
+      phoenix.ui.suggestOptions['@'].push({ title: name, subtitle: u.getOtherNames(phoenix, phoenix.users.profiles[k]) + ' ' + u.shortString(k), value: name })
+    }
+
+    // re-route to setup if needed
+    if (!phoenix.users.names[phoenix.user.id]) {
+      if (window.location.hash != '#/setup') {      
+        window.location.hash = '#/setup'
+        return
+      }
+    } else if (window.location.hash == '#/setup') {
+      window.location.hash = '#/'
+      return
+    }
+
+    // render the page
+    h.cleanup()    
+    var page = pages[phoenix.page.id]
+    if (!page)
+      page = pages.notfound
+    page(phoenix)
+  })
+}
+
+var newMessageCount = 0
+function getNewMessageCount() {
+  return newMessageCount
+}
+function setNewMessageCount (n) {
+  newMessageCount = n
+  if (n)
+    document.title = '('+n+') secure scuttlebutt'
+  else
+    document.title = 'secure scuttlebutt'
+}
+
+function setPage (name, page, opts) {
+  var el = document.getElementById('page-container')
+  el.innerHTML = ''
+  if (!opts || !opts.noHeader)
+    el.appendChild(com.page(phoenix, name, page))
+  else
+    el.appendChild(h('#page.container-fluid.'+name+'-page', page))
+  resizeControls()
+}
+
