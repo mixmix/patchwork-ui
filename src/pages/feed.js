d3d956cad54d458d19854574f609b8ef78978c55
Paul Frazee <pfrazee@gmail.com>
2015-04-23 17:41:38 -0500

    remove old search functionality from feeds

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -7,31 +7,9 @@ var com = require('../com')
 
 module.exports = function (app) {
 
-  var queryStr = app.page.qs.q || ''
-  var feedState = com.messageFeed.makeStateObj()
-  var feedFn = app.ssb.phoenix.createFeedStream
-
-  function filterFn (msg) {
-    var a = msg.value.author
-    var c = msg.value.content
-
-    if (!queryStr)
-      return true
-
-    var author = app.users.names[a] || a
-    var regex = new RegExp(queryStr.replace(/\s/g, '|'))
-    if (regex.exec(author) || regex.exec(c.type))
-      return true
-    if (c.type == 'post' && regex.exec(c.text))
-      return true
-    return false
-  }
-
   // markup
 
-  var searchInput = h('input.search', { type: 'text', placeholder: 'Search', value: queryStr })
-  var composeContainer = h('div')
-  var feed = com.messageFeed(app, { feed: feedFn, filter: filterFn, state: feedState, infinite: true })
+  var feed = com.messageFeed(app, { infinite: true })
   app.setPage('feed', h('.row',
     h('.col-xs-1'),
     h('.col-xs-7', feed),
@@ -40,16 +18,4 @@ module.exports = function (app) {
       com.sidehelp(app)
     )
   ))
-
-  function makeUri (opts) {
-    opts.q = ('q' in opts) ? opts.q : queryStr
-    return '#/feed?q=' + encodeURIComponent(opts.q)
-  }
-
-  // handlers
-
-  function onsearch (e) {
-    e.preventDefault()
-    window.location.hash = makeUri({ q: searchInput.value })
-  }
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -8,14 +8,11 @@ var com = require('../com')
 module.exports = function (app) {
 
   var myprofile = app.users.profiles[app.user.id]
-  var queryStr = app.page.qs.q || ''
-  var feedState = com.messageFeed.makeStateObj()
-  var feedFn = app.ssb.phoenix.createFeedStream
-
   function filterFn (msg) {
     var a = msg.value.author
     var c = msg.value.content
 
+    // posts only
     if (c.type !== 'post')
       return false
 
@@ -23,23 +20,12 @@ module.exports = function (app) {
     if (a !== app.user.id && (!myprofile.assignedTo[a] || !myprofile.assignedTo[a].following))
       return false
 
-    if (!queryStr)
-      return true
-
-    var author = app.users.names[a] || a
-    var regex = new RegExp(queryStr.replace(/\s/g, '|'))
-    if (regex.exec(author) || regex.exec(c.type))
-      return true
-    if (c.type == 'post' && regex.exec(c.text))
-      return true
-    return false
+    return true
   }
 
   // markup
 
-  var searchInput = h('input.search', { type: 'text', placeholder: 'Search', value: queryStr })
-  var composeContainer = h('div')
-  var feed = com.messageFeed(app, { feed: feedFn, filter: filterFn, state: feedState, infinite: true })
+  var feed = com.messageFeed(app, { filter: filterFn, infinite: true })
   app.setPage('feed', h('.row',
     h('.col-xs-1'),
     h('.col-xs-7', feed),
@@ -48,16 +34,4 @@ module.exports = function (app) {
       com.sidehelp(app)
     )
   ))
-
-  function makeUri (opts) {
-    opts.q = ('q' in opts) ? opts.q : queryStr
-    return '#/feed?q=' + encodeURIComponent(opts.q)
-  }
-
-  // handlers
-
-  function onsearch (e) {
-    e.preventDefault()
-    window.location.hash = makeUri({ q: searchInput.value })
-  }
 }
