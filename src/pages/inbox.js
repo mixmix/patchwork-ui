bdb4e38c9269e2339b2f1fb266da346e45af51f4
Paul Frazee <pfrazee@gmail.com>
2015-04-25 10:37:21 -0500

    only highlight unread messages in the inbox page

--- src/pages/inbox.js
+++ src/pages/inbox.js
@@ -12,7 +12,7 @@ module.exports = function (app) {
   // markup
 
   var feed = com.messageFeed(app, { feed: feedFn, infinite: true })
-  app.setPage('feed', h('.row',
+  app.setPage('inbox', h('.row',
     h('.col-xs-1'),
     h('.col-xs-7', feed),
     h('.col-xs-3.right-column.full-height',
