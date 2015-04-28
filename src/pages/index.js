22d71763a3cf34794841a77ca854e61518676ae5
Paul Frazee <pfrazee@gmail.com>
2015-04-27 20:44:02 -0500

    style notfound page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,13 @@ var h = require('hyperscript')
 var com = require('../com')
 
 function notfound (app) {
-  app.setPage('notfound', h('.row',
-    h('.col-xs-2.col-md-1', com.sidenav(app)),
-    h('.col-xs-10.col-md-11', h('p', h('strong', 'Not Found')))
-  ))
+  app.setPage('notfound', h('.row', h('.col-xs-push-2.col-xs-8',
+    h('img', { src: '/img/lick-the-door.gif', style: 'display: block; margin: 10px auto; border-radius: 3px;' }),
+    h('h2.text-muted.text-center', '404 Not Found'),
+    h('div.text-center', { style: 'margin-top: 20px' },
+      h('span', { style: 'background: #fff; border: 1px solid #ccc; padding: 1em' },
+        'Sorry, that page wasn\'t found. Maybe you typed the URL wrong? Or maybe somebody gave you a bad link.'))
+  )))
 }
 
 module.exports = {
