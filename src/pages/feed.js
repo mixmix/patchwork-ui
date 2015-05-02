1f8fd231efc5ae717bfa1785d60e72ca46a105c1
Paul Frazee <pfrazee@gmail.com>
2015-05-02 16:45:10 -0700

    move com/composer.header out of com/message-feed

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -9,10 +9,11 @@ module.exports = function (app) {
 
   // markup
 
-  var feed = com.messageFeed(app, { infinite: true })
   app.setPage('feed', h('.row',
     h('.col-xs-1'),
-    h('.col-xs-7', feed),
+    h('.col-xs-7', 
+      h('.header-ctrls', com.composer.header(app)),
+      com.messageFeed(app, { infinite: true })),
     h('.col-xs-3.right-column',
       h('.right-column-inner', com.friendsHexagrid(app)),
       com.sidehelp(app)
