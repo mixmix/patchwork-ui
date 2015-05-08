54e158a5990efe11608f99373eef6537f68e2ea9
Paul Frazee <pfrazee@gmail.com>
2015-05-08 11:16:47 -0500

    remove 'invite codes' from add-contact prompt

--- src/app-ui/index.js
+++ src/app-ui/index.js
@@ -1,5 +1,6 @@
 var h = require('hyperscript')
 var schemas = require('ssb-msg-schemas')
+var mlib = require('ssb-msgs')
 
 module.exports = function (phoenix) {
 
@@ -30,55 +31,22 @@ module.exports = function (phoenix) {
   phoenix.ui.followPrompt = function (e) {
     e.preventDefault()
 
-    var id = prompt('Enter the contact id or invite code')
+    var id = prompt('Enter the contact id')
     if (!id)
       return
+    if (!mlib.isHash(id))
+      return swal('Invalid ID', '"'+id+'" is not a valid user ID. Please check your source and try again.', 'error')
 
-    // surrounded by quotes?
-    // the scuttlebot cli ouputs invite codes with quotes, so this could happen
-    if (id.charAt(0) == '"' && id.charAt(id.length - 1) == '"')
-      id = id.slice(1, -1) // strip em
-
-    var parts = id.split(',')
-    var isInvite = (parts.length === 3)
-    if (isInvite) {
-      phoenix.ui.setStatus('info', 'Contacting server with invite code, this may take a few moments...')
-      phoenix.ssb.invite.addMe(id, next)
-    }
-    else
-      schemas.addContact(phoenix.ssb, id, { following: true }, next)
-      
-    function next (err) {
-      phoenix.ui.setStatus(false)
+    schemas.addContact(phoenix.ssb, id, { following: true }, function (err) {
       if (err) {
         console.error(err)
-        if (isInvite)
-          swal('Invite Code Failed', userFriendlyInviteError(err.stack || err.message), 'error')
-        else
-          swal('Error While Publishing', err.message, 'error')
+        swal('Error While Publishing', err.message, 'error')
       }
       else {
-        if (isInvite)
-          swal('Invite Code Accepted', 'You are now hosted by '+parts[0], 'success')
-        else
-          swal('Contact Added', 'You will now follow the messages published by your new contact.', 'success')
+        swal('Contact Added', 'You will now follow the messages published by your new contact.', 'success')
         phoenix.refreshPage()
       }
-    }
-
-    function userFriendlyInviteError(msg) {
-      if (~msg.indexOf('incorrect or expired') || ~msg.indexOf('has expired'))
-        return 'Invite code is incorrect or expired. Make sure you copy/pasted it correctly. If you did, ask the pub-server owner for a new code and try again.'
-      if (~msg.indexOf('invalid') || ~msg.indexOf('feed to follow is missing') || ~msg.indexOf('may not be used to follow another key'))
-        return 'Invite code is malformed. Make sure you copy/pasted it correctly. If you did, ask the pub-server owner for a new code and try again.'
-      if (~msg.indexOf('pub server did not have correct public key'))
-        return 'The pub server did not identify itself correctly for the invite code. Ask the pub-server owner for a new code and try again.'
-      if (~msg.indexOf('unexpected end of parent stream'))
-        return 'Failed to connect to the pub server. Check your connection, make sure the pub server is online, and try again.'
-      if (~msg.indexOf('already following'))
-        return 'You are already followed by this pub server.'
-      return 'Sorry, an unexpected error occurred. Please try again.'
-    }
+    })
   }
 
   phoenix.ui.setNamePrompt = function (userId) {
