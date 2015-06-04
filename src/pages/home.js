de429306e63a9699840e0875c60c4b202decae1f
Paul Frazee <pfrazee@gmail.com>
2015-06-04 11:55:36 -0500

    3-col home page, fully cicle avi

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,11 +9,12 @@ module.exports = function (app) {
 
   // markup
 
-  app.setPage('home', h('.layout-twocol',
+  app.setPage('home', h('.layout-threecol',
+    h('.layout-leftnav'),
     h('.layout-main', 
       com.welcomehelp(app),
       com.messageFeed(app, { feed: app.ssb.phoenix.createHomeStream, loadmore: true, infinite: true })),
-    h('.layout-sidenav',
+    h('.layout-rightnav',
       com.networkGraph(app, { drawLabels: false, touchEnabled: false, mouseEnabled: false, mouseWheelEnabled: false }),
       com.friendsHexagrid(app, { size: 80 }),
       com.sidehelp(app)
