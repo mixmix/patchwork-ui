420af6bbcbd48afd19a4daf0103b0f3e2ec0c355
Paul Frazee <pfrazee@gmail.com>
2015-07-21 12:43:45 -0500

    sandbox webview navigations

--- webview-preload.js
+++ webview-preload.js
@@ -35,6 +35,33 @@ pull(ipcPush, rpcStream, pull.drain(
   function (err) { if (err) { console.error(err) } }
 ))
 
+// sandbox
+
+// prevent navigations through default behavior
+var location = window.document.location
+var preventNavigation = function (e) {
+  var originalHashValue = location.hash
+  window.setTimeout(function () {
+    location.hash = 'preventNavigation' + ~~ (9999 * Math.random())
+    location.hash = originalHashValue
+  }, 0)
+}
+window.addEventListener('beforeunload', preventNavigation, false)
+window.addEventListener('unload', preventNavigation, false)
+
+// send navigations to the parent frame
+window.addEventListener('click', function (e) {
+  var el = e.target
+  while (el) {
+    if (el.tagName == 'A') {
+      console.log('navigate', el.getAttribute('href'))
+      ipc.sendToHost('navigate', el.getAttribute('href'))
+      return
+    }
+    el = el.parentNode
+  }
+}, false)
+
 // exports
 
 window.pull = pull
